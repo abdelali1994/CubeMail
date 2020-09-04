6985023529210f811a63acb1ab3cb079a0c9ca8c
abdelali1994 <abdelalighaffari@gmail.com>
2020-09-04 14:01:05 +0200

    Updates

---
+++ precache-manifest.18a8d9c1b735c848143fb73c609a4ece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264018ad2285c5173bd9dbd8f15c05bd",
+    "url": "/CubeMail/index.html"
+  },
+  {
+    "revision": "ed9ef8ca666f64dcecf6",
+    "url": "/CubeMail/static/js/2.8f53cc88.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/CubeMail/static/js/2.8f53cc88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67cb90dec5b58497255b",
+    "url": "/CubeMail/static/js/main.23319ca5.chunk.js"
+  },
+  {
+    "revision": "02c88928f9594843aaaa",
+    "url": "/CubeMail/static/js/runtime-main.e3af2818.js"
+  },
+  {
+    "revision": "c9034baf5e3a91a16cd1d3ba9de5b925",
+    "url": "/CubeMail/static/media/smiyasahla.c9034baf.svg"
+  }
+]);
